0e1b175f92cb37fd711c6db26e9f5f7aa722adfd
Abdelrhman Elruby <abdelrhmantr@gmail.com>
2024-02-11 19:16:47 +0200

    modified:   Subtitle-Renamer.py

--- Subtitle-Renamer.py
+++ Subtitle-Renamer.py
@@ -12,51 +12,42 @@ def vid_extn():
     print("\n\t"+"-"*25)
     print("\tExtension of video files")
     print("\t"+"-"*25)
-    print("\t1. mkv")
-    print("\t2. mp4")
-    print("\t3. avi")
-    print("\t4. else")
+    print("\t1. mkv | mp4 | avi")
+    print("\t2. else")
     while 1>0:
-        num = int(input("\n\tEnter extension (1-4): "))
+        extn = int(input("\n\tEnter extension (1-2): "))
         print("\t"+"-"*25)
-        if num in[1,2,3,4]:
+        if extn in[1,2]:
             break
-        else: print("\t"+'chosen number is incorrect')
+        else: print('\tchosen number is incorrect')
     
-    
-    if num==4:
-        num=str(input("Enter the extension of video file(ex: .mp4): "))
-    else: 
-        num=['.mkv','.mp4','.avi'][num-1]
-    return num
+    if extn==2:
+        extn=str(input("\n\tEnter the extension of video file(ex: .mp4): "))
+    return extn
 
 def sub_extn():
     print("\n\t"+"-"*25)
     print("\tExtension of subtitle files")
     print("\t"+"-"*25)
-    print("\t1. srt")
-    print("\t2. ass")
-    print("\t3. txt")
-    print("\t4. else")
+    print("\t1. srt | ass | txt | sub")
+    print("\t2. else")
     while 1>0:
-        num = int(input("\n\tEnter extension (1-4): "))
+        extn = int(input("\n\tEnter extension (1-2): "))
         print("\t"+"-"*25)
-        if num in[1,2,3,4]:
+        if extn in[1,2]:
             break
-        else: print("\t"+'chosen number is incorrect')
+        else: print('\tchosen number is incorrect')
     
     
-    if num==4:
-        num=str(input("Enter the extension of subtitle file(ex: .mp4): "))
-    else: 
-        num=['.srt','.ass','.txt'][num-1]
-    return num
+    if extn==2:
+        extn=str(input("\n\tEnter the extension of subtitle file(ex: .srt): "))
+    return extn
 
 print("\n\t"+"*"*60)
-print("\n\t Make sure subtiles and videos are in the same diractory.")
+print("\n\tMake sure subtiles and videos are in the same diractory.")
 print("\n\t"+"*"*60)
 
-dir = str(input("\n\t Full path to video directory(example: E:\media\Shows\...): "))
+dir = str(input("\n\tFull path to video directory(example: E:\media\Shows\...): "))
 
 vid_format = vid_extn()
 sub_format = sub_extn()
@@ -64,78 +55,74 @@ vid=[]
 sub=[]
 
 for name in os.listdir(dir):
-    if name[-4:]==vid_format:
-        vid.append(name[:-4])
-    elif name[-4:]==sub_format:
-        sub.append(name[:-4])
+    if vid_format==1:
+        if name.endswith('.mp4') or name.endswith('.mkv') or name.endswith('.avi'):
+            vid.append(name)
+    elif name.endswith(vid_format):
+            vid.append(name)
 
+    if sub_format==1:
+        if name.endswith('.srt') or name.endswith('.ass') or name.endswith('.txt') or name.endswith('.sub'):
+            sub.append(name)
+    elif name.endswith(sub_format):
+            sub.append(name)
 
-eps_num=[]
-sub_eps_num=[]
+if not vid:
+    raise SystemExit("\tNo video files were found with the selected extension")
+if not sub:
+    raise SystemExit("\tNo subtile files were found with the selected extension")
 
-for eps in vid:
-    for n in [6,5,4,3,2,1]:
-        try:
-            eps=re.findall('e'+'[0-9]'*n,eps.lower())[0]
-            break
-        except:
-            pass
-        eps=re.sub('[0-9][0-9][0-9][0-9]p', '', eps.lower()) #1080p
-        eps=re.sub('[0-9][0-9][0-9]p', '', eps) #720p
-        eps=re.sub('[0-9][0-9]bit', '', eps) #10bit
-        eps=re.sub('[0-9]bit', '', eps) #8bit
-        eps=re.sub('x[0-9][0-9][0-9]', '', eps) #X265
-        eps=re.sub('s[0-9][0-9]', '', eps) #s05
-        eps=re.sub('s[0-9]', '', eps) #s5
-        eps=re.sub('av1', '', eps) #AV1
-        eps=re.sub('[0-9]ch', '', eps) #2ch
-        eps=re.sub('DDP[0-9].[0-9]', '', eps) #DDP5.1
-        eps=re.sub('ion[0-9][0-9]', '', eps) #ION10
-        eps=re.sub('20[0-9][0-9]', '', eps) #2019
-        eps=re.sub('19[0-9][0-9]', '', eps) #1999
-        eps=re.sub('hdr[0-9][0-9]', '', eps) #hdr
-    eps_num.append(re.sub('\D', '', eps).lstrip("0"))
+def get_num(sorce):
+    num=[]
+    for eps in sorce:
+        for n in [6,5,4,3,2,1]:
+            try:
+                eps=re.findall('e'+'[0-9]'*n,eps.lower())[0] 
+                break
+            except:
+                if n != 1:
+                    continue
+                else: pass
+            eps=re.sub('[0-9][0-9][0-9][0-9]p', '', eps.lower()) #1080p
+            eps=re.sub('[0-9][0-9][0-9]p', '', eps) #720p
+            eps=re.sub('[0-9][0-9]bit', '', eps) #10bit
+            eps=re.sub('[0-9]bit', '', eps) #8bit
+            eps=re.sub('x[0-9][0-9][0-9]', '', eps) #X265
+            eps=re.sub('s[0-9][0-9]', '', eps) #s05
+            eps=re.sub('s[0-9]', '', eps) #s5
+            eps=re.sub('av1', '', eps) #AV1
+            eps=re.sub('[0-9]ch', '', eps) #2ch
+            eps=re.sub('DDP[0-9].[0-9]', '', eps) #DDP5.1
+            eps=re.sub('ion[0-9][0-9]', '', eps) #ION10
+            eps=re.sub('20[0-9][0-9]', '', eps) #2019
+            eps=re.sub('19[0-9][0-9]', '', eps) #1999
+            eps=re.sub('hdr[0-9][0-9]', '', eps) #hdr
+        num.append(re.sub('\D', '', eps).lstrip("0"))
+    return num
 
-for eps in sub:
-    for n in [6,5,4,3,2,1]:
-        try:
-            eps=re.findall('e'+'[0-9]'*n,eps.lower())[0]
-            break
-        except:
-            pass
-        eps=re.sub('[0-9][0-9][0-9][0-9]p', '', eps.lower()) #1080p
-        eps=re.sub('[0-9][0-9][0-9]p', '', eps) #720p
-        eps=re.sub('[0-9][0-9]bit', '', eps) #10bit
-        eps=re.sub('[0-9]bit', '', eps) #8bit
-        eps=re.sub('x[0-9][0-9][0-9]', '', eps) #X265
-        eps=re.sub('s[0-9][0-9]', '', eps) #s05
-        eps=re.sub('s[0-9]', '', eps) #s5
-        eps=re.sub('av1', '', eps) #AV1
-        eps=re.sub('[0-9]ch', '', eps) #2ch
-        eps=re.sub('DDP[0-9].[0-9]', '', eps) #DDP5.1
-        eps=re.sub('ion[0-9][0-9]', '', eps) #ION10
-        eps=re.sub('20[0-9][0-9]', '', eps) #2019
-        eps=re.sub('19[0-9][0-9]', '', eps) #1999
-        eps=re.sub('hdr[0-9][0-9]', '', eps) #hdr
-    sub_eps_num.append(re.sub('\D', '', eps).lstrip("0"))
+eps_num=get_num(vid)
+sub_eps_num=get_num(sub)
 
 if len(vid) != len(sub):
     print('\t')
-    print("\t!!!!! number of videos: ",len(vid))
-    print("\t!!!!! number of subtitles: ",len(sub),'\n')
+    print("\t!!!!! number of videos: ",len(vid)+"!!!!!")
+    print("\t!!!!! number of subtitles: ",len(sub)+"!!!!!",'\n')
     print("\t"+"*"*25+'\n')
     
 
 rename=0   
 miss_sub=[]
+print(sub_eps_num)
+print(eps_num)
+
 for i in range(len(vid)):
     for X in range(len(sub)):
         if eps_num[i] == sub_eps_num[X] :
-            os.rename(os.path.join(dir,sub[X]+sub_format),os.path.join(dir,vid[i]+sub_format))
+            os.rename(os.path.join(dir,sub[X]),os.path.join(dir,vid[i][:-4]+sub[X][-4:]))
             rename+=1
             break
         elif X == len(sub)-1:
-            print("\t!!!!! episode number",eps_num[i],"has no subtitle.")
+            print("\t!!!!! episode number",eps_num[i],"has no subtitle."+"!!!!!")
             miss_sub.append(vid[i])
 
 if len(vid) != rename:
